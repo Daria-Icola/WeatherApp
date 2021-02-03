9b3d54738eb93ddc161bd7dca3b51df72a28fa5b
Daria-Icola <42102070+Daria-Icola@users.noreply.github.com>
2021-02-03 15:45:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,10 +14,6 @@ pip install pyowm
 # Установка библиотеки с датой
 pip install DateTime
 
-
-#####################
-# Установка Node.js
-curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
 # Установка Electron
 npm install --save-dev electron
 
